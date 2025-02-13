3af8add3bc3470e3fa8a5ab52fdf5b4a92b3377c
Máté Fehér <c9fehmat@vasvari.org>
2025-02-13 09:22:17 +0100

    ClearForm

--- Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
+++ Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
@@ -50,5 +50,10 @@ namespace Kreta.Desktop.ViewModels.SchoolSubjects
             //2.a A megérkezett adatokkal újra létrehozzuk a Subjects ObservableCollection
             Subjects = new ObservableCollection<Subject>(subjects);
         }
+        private void ClearForm()
+        {
+            SelectedSubject = new Subject();
+            OnPropertyChanged(nameof(SelectedSubject));
+        }
     }
 }
